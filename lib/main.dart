124d6ad2b5f579432a05673995e227b847298759
malazhariy <mostafa.maok2000@gmail.com>
2024-04-06 00:40:29 +0200

    correct init EasyLocalization in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spacex/core/di/dependency_injection.dart';
 import 'package:spacex/core/utils/common/bloc_observer.dart';
+import 'package:spacex/core/utils/common/languages.dart';
 import 'package:spacex/core/utils/common/system_overlay_style.dart';
 import 'package:spacex/my_app.dart';
 import 'package:flutter/material.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +18,18 @@ void main() async {
     DI.init(),
   ]);
 
-
   Bloc.observer = const MyBlocObserver();
-
 
   // set system app bar style
   SystemChrome.setSystemUIOverlayStyle(kSystemOverlayStyle);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-  runApp(const MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: Language.locales,
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const MyApp(),
+    ),
+  );
 }
