9523a6d538c338bd0f74c405afbc6b89fd10944c
malazhariy <mostafa.maok2000@gmail.com>
2024-04-17 09:18:36 +0200

    [add] lottie assets

--- lib/core/resources/app_images.dart
+++ lib/core/resources/app_images.dart
@@ -7,3 +7,15 @@ class AppImages {
   static const String arFlag = "$_baseImagePath/ar.png";
   static const String enFlag = "$_baseImagePath/en.png";
 }
+
+class AppLottie {
+  static const String _baseLottiePath = "assets/lottie";
+
+  static const String splashBackground = "$_baseLottiePath/splash_background.json";
+  static const String skyWithStars = "$_baseLottiePath/sky_with_star.json";
+  static const String skyWithStars2 = "$_baseLottiePath/sky_with_star2.json";
+  static const String blackSkyWithStars = "$_baseLottiePath/black_sky_with_star.json";
+  static const String loadingPlanet = "$_baseLottiePath/loading_planet.json";
+  static const String circlesPlanetLoading = "$_baseLottiePath/circles_planet_loading.json";
+  static const String astronautNoInternet = "$_baseLottiePath/astro_no_internet.json";
+}
